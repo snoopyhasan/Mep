3dbab1d20db32795a1d43127f8f07594475febca
snoopyhasan <100765315+snoopyhasan@users.noreply.github.com>
2022-12-25 14:40:11 +0530

    Added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import Router from 'next/router';
 import { initGA, logPageView } from 'analytics';
+import Script from 'next/script';
 // Load DM Sans typeface
 import 'typeface-dm-sans';
 import 'components/style.css';
@@ -21,6 +22,19 @@ export default function CustomApp({ Component, pageProps }) {
     logPageView();
     Router.events.on('routeChangeComplete', logPageView);
   }, []);
+  
+  
+  
+<!-- Google tag (gtag.js) -->
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-SD43VJXWZS"></Script>
+<Script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SD43VJXWZS');
+</Script>
+
 
   return <Component {...pageProps} />;
 }
